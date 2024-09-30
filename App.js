53364cca678c39d260371f12246907d12d078309
asuf29 <asuf29@icloud.com>
2024-09-30 23:52:36 +0300

    install tw

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello, world!</Text>
+      <Text style={tw`text-red-500`}>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   );
