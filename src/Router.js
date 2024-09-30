1d4acd5db00790aaa38aa0df65fc19e555185980
asuf29 <asuf29@icloud.com>
2024-10-01 02:26:29 +0300

    chatGPT screen added to router

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './screens/WelcomeScreen';
 import HomeScreen from './screens/HomeScreen';
+import ChatGPT from './screens/ChatGPT';
+
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
       >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="chatGPT" component={ChatGPT} />
       </Stack.Navigator>
     </NavigationContainer>
   );
