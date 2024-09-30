85703a50b30e5356ed93916108f8cbd77b31a05e
asuf29 <asuf29@icloud.com>
2024-10-01 01:43:34 +0300

    home added to router

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './screens/WelcomeScreen';
-
+import HomeScreen from './screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
         }}
       >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
