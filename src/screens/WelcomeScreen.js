7d94864b83216a2e0a6940946e3a821ca6c346e9
asuf29 <asuf29@icloud.com>
2024-10-01 00:03:38 +0300

    create welcome screen

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function WelcomeScreen() {
+  return (
+    <View>
+      <Text>WelcomeScreen</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
